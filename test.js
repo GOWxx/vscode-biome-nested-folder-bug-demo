51d42a0028a3d21cba7e61b8b722ad2f4a86a8b8
GOWxx <sunbowen523@gmail.com>
2024-04-05 01:14:52 +0800

    feat: add demo

---
+++ test.js
@@ -0,0 +1,6 @@
+const arrayarrayarrayarrayarrayarray = [];
+const array2array2array2array2array2 = [];
+console.log(
+	"long long long long long long long text" ===
+		"notshort short short short short short text",
+);
